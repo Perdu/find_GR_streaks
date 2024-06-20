d634b54aad58dcd6336b5dc4d4b5f48c426dd078
Célestin Matte <dev@cmatte.me>
2024-06-20 17:43:55 +0200

    Add SCRIPT_DIR to allow executing from another folder

--- look_for_streaks.sh
+++ look_for_streaks.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 if [ $# -eq 0 ]; then
     echo "Usage: $0 <video_recording.mp4>"
     exit 1
@@ -9,6 +11,6 @@ for file in "$@"; do
     # echo "Converting audio..."
     ffmpeg -y -loglevel quiet -i "$file" -q:a 0 -map a /tmp/output_audio.wav
     # echo "Transcribing audio..."
-    python transcribe_with_whisper.py $(basename "$file")
+    python $SCRIPT_DIR/transcribe_with_whisper.py $(basename "$file")
     echo
 done
